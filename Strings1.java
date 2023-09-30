92d233a1a75fe4c7293b3e00163d61848a786068
ayocloudi <126922387+ayocloudi@users.noreply.github.com>
2023-09-30 22:36:45 +0100

    Create Strings1.java
    This code defines a class Strings1 that, when executed, initializes two strings s1 with the value "Computer Science" and s2 with the value "Education". It then extracts a substring from s1 starting from index 9 up to (but not including) index 16, which results in the word "Science". Finally, it prints the extracted substring followed by the string "Education", resulting in the output "Science Education"

---
+++ Strings1.java
@@ -0,0 +1,15 @@
+class Strings1 { 
+
+	public static void main(String [] args) {
+
+		String s1 = "Computer Science";
+		String s2 = "Education";
+
+		String s3 = s1.substring(9,16);
+		System.out.print(s3 + " " + s2);
+		
+
+
+	}
+	
+}
